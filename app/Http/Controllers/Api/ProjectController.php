864affffb84b4e5fe27ca50b6fdb3860718a6cb1
Bernardo Greco <Bernardo.greco98@gmail.com>
2023-05-04 16:24:37 +0200

    FEAT api show route added

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,10 +11,27 @@ class ProjectController extends Controller
     public function index()
     {
 
-        $results = Project::with('type:id,name', 'type.projects', 'technologies:name')->get();
+        $results = Project::with('type.projects')->get();
 
         return response()->json([
             'results' => $results,
         ]);
+    }
+
+    public function show($slug)
+    {
+        $project = Project::where('slug', $slug)->first();
+
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'project' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'No project found'
+            ]);
+        }
     }
 }
